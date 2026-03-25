c1fa40ef7be4094207df7e3bf5e0734ed6dac09b
Mbutu <mbutu.mbutu@outlook.com>
2026-03-25 17:18:04 +0100

    Less is more, removed some cmake flags

--- init.bat
+++ init.bat
@@ -1,11 +1,7 @@
-:: Use -DBUILD_TESTS=ON to build with testSolution
-:: The file "compile_commands" belongs in the project root directory, but if you disagree, by all means, add 
+:: The file "compile_commands" belongs in the project root directory, imho 
 ::
-:: require('lspconfig').clangd.setup{ init_options = {compilationDatabasePath = './build/'}}
-::
-:: to lua.init or something similar.
 
-cmake -S . -B build -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang -DBUILD_TESTS=OFF
+cmake -S . -B build -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang 
 copy build\compile_commands.json .
 cmake --build build
 
